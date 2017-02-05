bfb3217fbe6997606688b543062d8b259425ebb9
Dale Bustad <dale@divmain.com>
2017-02-04 23:28:36 -0800

    Components that returned null would not render correctly (see big fat comment).
    Closes #8.

--- src/index.js
+++ src/index.js
@@ -106,9 +106,56 @@ function evalComponent (node, context) {
 
   const childContext = getChildContext(node.type, instance, context);
 
-  return isFunction(instance.render) ?
-    traverse(instance.render(), childContext) :
-    traverse(instance, childContext);
+  if (isFunction(instance.render)) {
+    return traverse(instance.render(), childContext);
+  }
+
+  /*
+    The following check looks really strange... why _wouldn't_ `instance`
+    be an instance of `node.type`?  The answer has to do with JavaScript's
+    behavior when values are returned from a constructor invoked with `new`.
+    If an object is returned from the constructor, the function is treated
+    as if it had been invoked without `new`.
+
+    This behavior is defined in section 9.2.2 step 13a of the ECMAScript
+    Language Specification, 7th Edition, here:
+    http://www.ecma-international.org/ecma-262/7.0/index.html#sec-ecmascript-function-objects-construct-argumentslist-newtarget
+
+    Specifically relevant is section 9.2.2 step 13a.
+
+    However, in the circumstance of object construction, a `null` return value
+    is treated the same as `undefined` and an empty object with prototype
+    Constructor will be returned.
+
+    Here it is demonstrated in code:
+
+      const NotNullComponent = () => <div />;
+      const notNullComponentNode = <NotNullComponent />;
+      const notNullComponentNodeTypeInstance = new notNullComponentNode.type;
+      notNullComponentNodeTypeInstance instanceof notNullComponentNode.type;
+      // false
+      notNullComponentNodeTypeInstance;
+      // { '$$typeof': Symbol(react.element),
+      //   type: 'div',
+      //   key: null,
+      //   ref: null,
+      //   props: {},
+      //   _owner: null,
+      //   _store: {} }
+
+      const NullComponent = () => null;
+      const nullComponentNode = <NullComponent />;
+      const nullComponentNodeTypeInstance = new nullComponentNode.type;
+      nullComponentNodeTypeInstance
+      // NullComponent {}
+      nullComponentNodeTypeInstance instanceof nullComponentNode.type
+      // true
+  */
+  if (instance instanceof node.type) {
+    return most.empty();
+  }
+
+  return traverse(instance, childContext);
 }
 
 function traverse (node, context) {
