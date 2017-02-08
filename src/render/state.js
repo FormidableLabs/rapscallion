9332153926e983aef35623e9907803c21b95ff63
Dale Bustad <dale@divmain.com>
2017-02-08 01:21:54 -0800

    JSDoc the `src/render/` subdir.

--- src/render/state.js
+++ src/render/state.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-invalid-this */
 const {
   isFunction,
   assign
 } = require("lodash");
 
 
-/* eslint-disable no-invalid-this */
+/**
+ * A synchronous replacement for React's `setState` method.
+ *
+ * @param      {Function|Object}  newState  An object containing new keys/values
+ *                                          or a function that will provide the same.
+ * @param      {Function}         cb        A callback that may be provided to setState.
+ *
+ * @returns    {undefined}                  No return value.
+ */
 function syncSetState (newState, cb) {
   // Mutation is faster and should be safe here.
   this.state = assign(
@@ -15,7 +24,6 @@ function syncSetState (newState, cb) {
   );
   if (cb) { cb.call(this); }
 }
-/* eslint-enable no-invalid-this */
 
 
 module.exports = { syncSetState };
