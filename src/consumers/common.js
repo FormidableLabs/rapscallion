3c7795496f1db2a6d33246ed2e5c85494a9c0762
Dale Bustad <dale@divmain.com>
2017-02-08 02:05:50 -0800

    Add JSDocs for `src/consumers/*`.

--- src/consumers/common.js
+++ src/consumers/common.js
@@ -1,6 +1,20 @@
 const { EXHAUSTED } = require("../sequence");
 
 
+/**
+ * Given a batch size, request values from the source sequence and push them
+ * onto the provided "pushable" (a Node stream or an array).  Once the number
+ * of retrieved values reaches the specified batch size, return control back
+ * to the caller.
+ *
+ * @param      {Sequence}   sequence   Source sequence.
+ * @param      {Integer}    batchSize  The number of segments to generated before
+ *                                     returning control call to the caller.
+ * @param      {Array}      pushable   Destination for all segments.
+ *
+ * @return     {boolean}               Indicates whether there are more values to
+ *                                     be retrieved, or if this the last batch.
+ */
 function pullBatch (sequence, batchSize, pushable) {
   let iter = batchSize;
   while (iter--) {
