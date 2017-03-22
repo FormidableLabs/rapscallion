670a4907eb49f706948c3a1e74034ea1eada7f85
Patrick Williams <pwmckenna@gmail.com>
2017-03-21 20:54:49 -0700

    test case

--- spec/special-cases.js
+++ spec/special-cases.js
@@ -1,9 +1,15 @@
 import { default as React } from "react";
-
+import { checkParity, getRootNode } from "./_util";
 import { render } from "../src";
 
 
 describe("special cases", () => {
+  it("can handle [] in __html", () => {
+    checkParity(getRootNode(`
+      return () => <script dangerouslySetInnerHTML={{__html:[] }} type="text/javascript" />
+    `), {});
+  });
+
   it("renders components that return null", () => {
     const NullComponent = () => null;
     const Parent = () => <div><NullComponent /></div>;
