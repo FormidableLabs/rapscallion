e7e69e90b4d7d3b32a11bb4f46abea7d32816656
Dale Bustad <dale@divmain.com>
2017-07-05 13:18:37 -0700

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PULL_REQUEST_NUMBER=$(git show HEAD --format=format:%s | sed -nE 's/Merge pull request #([0-9]+).*/\1/p')
+
+if [ -z "$PULL_REQUEST_NUMBER" ]; then
+    echo "No pull request number found; aborting publish."
+else
+    echo "Detected pull request #$PULL_REQUEST_NUMBER."
+    SEMVER_CHANGE=$(curl "https://maintainerd.divmain.com/api/semver?repoPath=FormidableLabs/rapscallionz&installationId=37499&prNumber=$PULL_REQUEST_NUMBER")
+    if [ -z "$SEMVER_CHANGE" ]; then
+        echo "No semver selection found; aborting publish."
+    else
+        echo "Detected semantic version change of $SEMVER_CHANGE."
+
+        # CI might leave the working directory in an unclean state.
+        git reset --hard
+
+        eval npm version "$SEMVER_CHANGE"
+        npm publish
+
+        git config --global user.name "Dale Bustad (bot)"
+        git config --global user.email "dale@divmain.com"
+
+        git remote add origin-deploy https://${GH_TOKEN}@github.com/FormidableLabs/rapscallion.git > /dev/null 2>&1
+        git push --quiet --tags origin-deploy master
+
+        echo "Done!"
+    fi
+fi
